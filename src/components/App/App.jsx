eab1fc88559e8cf01d2a3feb9a893963cc25736a
Aileneboro <a.borovikova89@gmail.com>
2024-04-11 17:31:32 +0300

    styles

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,38 +3,46 @@ import Description from "../Description/Description";
 import Feedback from "../Feedback/Feedback";
 import Options from "../Options/Options";
 import Notification from "../Notification/Notification";
+import styles from "./App.module.css";
 
 const App = () => {
-  const getInitialFeedback = () => {
-    const savedFeedback = localStorage.getItem("feedback");
-    return savedFeedback
-      ? JSON.parse(savedFeedback)
-      : { good: 0, neutral: 0, bad: 0 };
-  };
+  const [feedbackTypes, setFeedbackTypes] = useState(() => {
+    const savedFeedback = JSON.parse(localStorage.getItem("feedback"));
+    if (savedFeedback !== null) {
+      return savedFeedback;
+    }
+    return {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    };
+  });
 
-  const [feedbackTypes, setFeedbackTypes] = useState(getInitialFeedback);
+  useEffect(() => {
+    localStorage.setItem("feedback", JSON.stringify(feedbackTypes));
+  }, [feedbackTypes]);
 
   const updateFeedback = (feedbackType) => {
-    setFeedbackTypes((prevFeedback) => ({
-      ...prevFeedback,
-      [feedbackType]: prevFeedback[feedbackType] + 1,
-    }));
+    setFeedbackTypes({
+      ...feedbackTypes,
+      [feedbackType]: feedbackTypes[feedbackType] + 1,
+    });
   };
 
   const handleReset = () => {
-    setFeedbackTypes({ good: 0, neutral: 0, bad: 0 });
+    setFeedbackTypes({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
   };
 
   const { good, neutral, bad } = feedbackTypes;
   const totalFeedback = good + neutral + bad;
   const positiveFeedbackPercentage = Math.round((good / totalFeedback) * 100);
 
-  useEffect(() => {
-    localStorage.setItem("feedback", JSON.stringify(feedbackTypes));
-  }, [feedbackTypes]);
-
   return (
-    <div>
+    <div className={styles.container}>
       <Description />
       <Options
         updateFeedback={updateFeedback}
