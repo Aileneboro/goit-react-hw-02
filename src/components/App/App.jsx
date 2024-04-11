20645e94c6acde2303aa6e246eb9d008132bf7a9
Aileneboro <a.borovikova89@gmail.com>
2024-04-11 16:52:04 +0300

    step6

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,34 +1,37 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Description from "../Description/Description";
 import Feedback from "../Feedback/Feedback";
 import Options from "../Options/Options";
 import Notification from "../Notification/Notification";
 
 const App = () => {
-  const [feedbackTypes, setFeedbackTypes] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  });
+  const getInitialFeedback = () => {
+    const savedFeedback = localStorage.getItem("feedback");
+    return savedFeedback
+      ? JSON.parse(savedFeedback)
+      : { good: 0, neutral: 0, bad: 0 };
+  };
 
-  const updateFeedback = (feedbackType, value = 1) => {
+  const [feedbackTypes, setFeedbackTypes] = useState(getInitialFeedback);
+
+  const updateFeedback = (feedbackType) => {
     setFeedbackTypes((prevFeedback) => ({
       ...prevFeedback,
-      [feedbackType]: prevFeedback[feedbackType] + value,
+      [feedbackType]: prevFeedback[feedbackType] + 1,
     }));
   };
 
   const handleReset = () => {
-    setFeedbackTypes({
-      good: 0,
-      neutral: 0,
-      bad: 0,
-    });
+    setFeedbackTypes({ good: 0, neutral: 0, bad: 0 });
   };
 
   const { good, neutral, bad } = feedbackTypes;
   const totalFeedback = good + neutral + bad;
   const positiveFeedbackPercentage = Math.round((good / totalFeedback) * 100);
+
+  useEffect(() => {
+    localStorage.setItem("feedback", JSON.stringify(feedbackTypes));
+  }, [feedbackTypes]);
 
   return (
     <div>
